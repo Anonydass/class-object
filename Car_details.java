89f4a4627d1a6bb7ad4e6ed2209220d13ac6bce8
Esakkiappan <59301579+Anonydass@users.noreply.github.com>
2022-11-30 16:28:21 -0800

    class file

---
+++ Car_details.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package car_details;
+
+/**
+ *
+ * @author E
+ */
+public class Car_details {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        car dass = new car ();
+        dass.model=50;
+        dass.brand_no = 48498;
+        dass.no_of_tyres = 4;
+        dass.type_engine_no = 7438;
+        dass.chase_no = 73483;
+        dass.display();
+        
+    }
+    
+}
