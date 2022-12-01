f17f2188ca04ca8a7e8409687dec243f92ddf59a
Esakkiappan <59301579+Anonydass@users.noreply.github.com>
2022-11-30 16:27:49 -0800

    class & object main

---
+++ car.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package car_details;
+
+/**
+ *
+ * @author E
+ */
+public class car {
+    int model;
+    int brand_no;
+    int no_of_tyres;
+    int type_engine_no;
+    int chase_no;
+    void display ()
+    {
+        System.out.println("model = "+ model + "brand no = " + brand_no + "no of tyres = " + no_of_tyres + "type_engine_no = " + type_engine_no + "chase_no = " + chase_no + "display_vehicle_brand_and_model" + brand_no + model + "engineno_and_chase_no = " + type_engine_no + chase_no);
+    }
+    
+}
